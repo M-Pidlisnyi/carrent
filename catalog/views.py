afce7dbcefcb8c9a2c21f597d69cf820300b3542
M-Pidlisnyi <mishapropatriot@gmail.com>
2021-09-12 18:56:07 +0300

    created home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import BodyStyle, Brand, Car, CarInstance
 
 # Create your views here.
+def index(request):
+    """view function form home page"""
+    carNum = Car.objects.all().count()
+    instancesNum = CarInstance.objects.all().count()
+
+    availableInstances = CarInstance.objects.filter(status__exact='a').count()
+
+    brandNum = Brand.objects.all().count()
+
+    context = {
+        'carNum': carNum,
+        'instancesNum': instancesNum,
+        'availableInstances': availableInstances,
+        'brandNum': brandNum
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
